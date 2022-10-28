abb757a22d70f5a76e22a6ecfba2ab355458960c
Reason Adhikari <72090971+adhikariReason@users.noreply.github.com>
2022-10-27 19:20:46 -0500

    minor changes

--- website/auth.py
+++ website/auth.py
@@ -52,7 +52,7 @@ def sign_up():
 			new_user = User(email=email, password = generate_password_hash(password1, method = "sha256"))
 			db.session.add(new_user)
 			db.session.commit()
-			login_user(user, remember=True)
+			#login_user(user, remember=True)
 			flash("Account Created", category='success')
 			return redirect(url_for('views.home'))
 
